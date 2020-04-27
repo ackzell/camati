6aacd3aaebd57a47a418dd0f22c008cee257bd9a
Axel Martínez <ackzell@gmail.com>
2020-04-27 17:46:25 -0500

    #11 Creating a lambda that connects to a mongo db and stores a document when reached.

---
+++ functions/store_file.js
@@ -0,0 +1,60 @@
+// const { MongoClient } = require('mongodb')
+import { MongoClient } from 'mongodb'
+
+exports.handler = function(event, context, callback) {
+  const DB_URL = process.env.DB_URL
+  const DB_NAME = process.env.DB_NAME
+  const DB_COLLECTION = process.env.DB_COLLECTION
+  // event and context provided by Netlify
+  // callback provided by us
+
+  /**
+   * event
+   * {
+      "path": "Path parameter",
+      "httpMethod": "Incoming request's method name"
+      "headers": {Incoming request headers}
+      "queryStringParameters": {query string parameters }
+      "body": "A JSON string of the request payload."
+      "isBase64Encoded": "A boolean flag to indicate if the applicable request payload is Base64-encode"
+      }
+   */
+
+  MongoClient.connect(
+    `${DB_URL}/${DB_NAME}`,
+    { useUnifiedTopology: true },
+    function(err, connection) {
+      if (err) {
+        console.log('Error while connecting', err)
+        callback(null, {
+          statusCode: 500,
+          body: JSON.stringify({ error: err })
+        })
+      }
+
+      const db = connection.db(DB_NAME)
+      const audiosCollection = db.collection(DB_COLLECTION)
+
+      audiosCollection
+        .insertOne({ hallo: 'Welt' })
+        .then(
+          (result) => {
+            callback(null, { statusCode: 200, body: JSON.stringify(result) })
+          },
+          (err) => {
+            console.log(err)
+            callback(null, { statusCode: 501, body: 'Could not insert' })
+          }
+        )
+        .finally(() => connection.close())
+
+      // audiosCollection.insertOne({ hello: 'world' }).then((result) => {
+      //   console.log(result)
+      //   callback(null, {
+      //     statusCode: 200,
+      //     body: JSON.stringify(result)
+      //   })
+      // })
+    }
+  )
+}
